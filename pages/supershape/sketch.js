aad84f62645e6b774c786b84db11aa91bf110a58
Case <case.ploeg@mail.utoronto.ca>
2018-01-21 13:41:13 -0500

    implementing bootstrap

---
+++ pages/supershape/sketch.js
@@ -0,0 +1,19 @@
+var increment;
+var num_points;
+
+function setup() {
+  // put setup code here
+    createCanvas(400, 400);
+    var num_points = 500
+    increment = num_points / num_points
+    stroke(255);
+    fill(255);
+    strokeWeight(4);
+    
+}
+
+function draw() {
+  // put drawing code here
+    background(0);
+    point(200, 200);
+}
